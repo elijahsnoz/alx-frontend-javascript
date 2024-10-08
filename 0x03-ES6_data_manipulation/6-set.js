dcf1036389479173be331a5ade91a75282785819
elijahsnoz <elijahsnoz@gmail.com>
2024-10-08 21:51:20 +0100

    data manipulation

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Ajayi elijah <https://github.com/elijahsnoz>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+    return new Set(array);
+  }
